319a887c35d450703524289a7daf3a4c7035fb7e
S.Saha <108742981+SoumabhaSaha15@users.noreply.github.com>
2023-06-29 12:14:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function main() {
-  
+
   const SECTIONS = [...document.getElementsByTagName('section')];
   [...document.getElementsByClassName('btn')].forEach((item, index, array) => {
     item.addEventListener("click", () => {
@@ -7,5 +7,13 @@
       SECTIONS[index].style.display = 'grid';
     });
   });
-  
+  const Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
+    });
+  }, {
+    threshold: 0,
+  });
+  [...document.querySelectorAll('div .hide')].forEach((element) => { Observer.observe(element); })
+
 })();
