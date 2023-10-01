ec2ff243aea2a24498c3297150a0be1e28ef7da1
S.Saha <108742981+SoumabhaSaha15@users.noreply.github.com>
2023-10-01 22:57:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,16 @@
       SECTIONS[index].style.display = 'grid';
     });
   });
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwbvfc8u2DmIAn90nEKs9EIlvFGZuCoCeloEwRdxOBJ8TiaDh4jIlfqzTDX4ockAbXc7A/exec';
+  const form = document.forms['submit-to-google-sheet'];
+  form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => window.alert('Success!', response))
+      .catch(error => window.alert('Error!', error.message));
+  });
+  
   const Observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       entry.target.classList.toggle('show', entry.isIntersecting);
