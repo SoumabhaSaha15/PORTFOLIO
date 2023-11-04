dfbf08660738bebfa66ec14f6ba8eafeb733354a
S.Saha <108742981+SoumabhaSaha15@users.noreply.github.com>
2023-11-04 12:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 (function main() {
 
-  const SECTIONS = [...document.getElementsByTagName('section')];
-  [...document.getElementsByClassName('btn')].forEach((item, index, array) => {
-    item.addEventListener("click", () => {
-      SECTIONS.forEach((i) => { i.style.display = 'none'; });
-      SECTIONS[index].style.display = 'grid';
-    });
-  });
+  const SHOW_ONLY = (element)=>{ 
+    SECTIONS.forEach((item) => { 
+      item.style.display = 'none'; 
+    }); 
+    element.style.display = 'grid';
+  }
+  const SECTIONS = [HOME,ABOUT,MY_PROJECT,CONTACT] = document.querySelectorAll("body main section");
+  const BTN = [HOME_BTN,ABOUT_BTN,MY_PROJECT_BTN,CONTACT_BTN]=[...document.getElementsByClassName('btn')];
 
+  HOME_BTN.addEventListener("click",(e)=>{location.hash = "#home";});
+  ABOUT_BTN.addEventListener("click",(e)=>{location.hash = "#about";});
+  MY_PROJECT_BTN.addEventListener("click",(e)=>{location.hash = "#my-projects";});
+  CONTACT_BTN.addEventListener("click",(e)=>{location.hash = "#contact";});
+
+  window.addEventListener("hashchange",(e)=>{
+    switch(location.hash){
+      case "#home":
+        SHOW_ONLY(HOME);
+      break;
+      case "#about":
+        SHOW_ONLY(ABOUT);
+      break;
+      case "#my-projects":
+        SHOW_ONLY(MY_PROJECT);
+      break;
+      case "#contact":
+        SHOW_ONLY(CONTACT);
+      break;
+    }
+  });
+  
   const scriptURL = 'https://script.google.com/macros/s/AKfycbwbvfc8u2DmIAn90nEKs9EIlvFGZuCoCeloEwRdxOBJ8TiaDh4jIlfqzTDX4ockAbXc7A/exec';
   const form = document.forms['submit-to-google-sheet'];
   form.addEventListener('submit',(e) => {
@@ -26,6 +49,5 @@
   });
   
   [...document.querySelectorAll('div .hide')].forEach((element) => { Observer.observe(element); });
-
   
 })();
