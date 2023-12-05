6274626be2263755745610c054476ed088e14806
S.Saha <108742981+SoumabhaSaha15@users.noreply.github.com>
2023-12-05 10:38:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
     element.style.display = 'grid';
   }
   const SECTIONS = [HOME,ABOUT,MY_PROJECT,CONTACT] = document.querySelectorAll("body main section");
-  const BTN = [HOME_BTN,ABOUT_BTN,MY_PROJECT_BTN,CONTACT_BTN]=[...document.getElementsByClassName('btn')];
+  const BTN = [HOME_BTN,ABOUT_BTN,MY_PROJECT_BTN,CONTACT_BTN]=[...document.querySelectorAll("footer a button.btn")];
   // HOME_BTN.addEventListener("click",(e)=>{location.hash = "#home";});
   // ABOUT_BTN.addEventListener("click",(e)=>{location.hash = "#about";});
   // MY_PROJECT_BTN.addEventListener("click",(e)=>{location.hash = "#my-projects";});
