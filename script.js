1c0b8282971fe5adb530a2c00646449c24b6e801
S.Saha <108742981+SoumabhaSaha15@users.noreply.github.com>
2023-12-05 09:59:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,34 +7,38 @@
     element.style.display = 'grid';
   }
   const SECTIONS = [HOME,ABOUT,MY_PROJECT,CONTACT] = document.querySelectorAll("body main section");
-  // const BTN = [HOME_BTN,ABOUT_BTN,MY_PROJECT_BTN,CONTACT_BTN]=[...document.getElementsByClassName('btn')];
+  const BTN = [HOME_BTN,ABOUT_BTN,MY_PROJECT_BTN,CONTACT_BTN]=[...document.getElementsByClassName('btn')];
   // HOME_BTN.addEventListener("click",(e)=>{location.hash = "#home";});
   // ABOUT_BTN.addEventListener("click",(e)=>{location.hash = "#about";});
   // MY_PROJECT_BTN.addEventListener("click",(e)=>{location.hash = "#my-projects";});
   // CONTACT_BTN.addEventListener("click",(e)=>{location.hash = "#contact";});
 
-  const PAGE_LOAD = (e)=>{
+  const LOAD_PAGE = (e)=>{
     switch(location.hash){
       case "#home","":
         SHOW_ONLY(HOME);
+        HOME_BTN.hover();
       break;
       case "#about":
         SHOW_ONLY(ABOUT);
+        ABOUT_BTN.hover();
       break;
       case "#my-projects":
         SHOW_ONLY(MY_PROJECT);
+        MY_PROJECT_BTN.hover()
       break;
       case "#contact":
         SHOW_ONLY(CONTACT);
       break;
       default:
         SHOW_ONLY(HOME);
+        CONTACT_BTN.hover();
       break;
     }
   }
 
-  window.addEventListener("hashchange",PAGE_LOAD);
-  window.addEventListener("load",PAGE_LOAD);
+  window.addEventListener("hashchange",LOAD_PAGE);
+  window.addEventListener("load",LOAD_PAGE);
   
   const scriptURL = 'https://script.google.com/macros/s/AKfycbwbvfc8u2DmIAn90nEKs9EIlvFGZuCoCeloEwRdxOBJ8TiaDh4jIlfqzTDX4ockAbXc7A/exec';
   const form = document.forms['submit-to-google-sheet'];
